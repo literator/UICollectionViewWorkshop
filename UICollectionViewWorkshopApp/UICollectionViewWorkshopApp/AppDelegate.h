bb9fe701f8d3e26a89181402c4d994c0716e8d4c
Maciej Oczko <maciek.oczko@polidea.com>
2014-07-22 21:18:45 +0200

    Created project

---
+++ UICollectionViewWorkshopApp/UICollectionViewWorkshopApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UICollectionViewWorkshopApp
+//
+//  Created by Maciej Oczko on 22.07.2014.
+//  Copyright (c) 2014 Mobile Workshops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
